0e51976e9f5a101671ec021eea2ac879066a2f36
Mike A. <git@mikealmel.ooo>
2024-09-12 00:23:11 +0200

    Update config flow to support adding devices

--- custom_components/findmy/config_flow.py
+++ custom_components/findmy/config_flow.py
@@ -4,12 +4,14 @@ from __future__ import annotations
 
 import logging
 import re
-from typing import TYPE_CHECKING, Callable, TypedDict
+from typing import TYPE_CHECKING, Callable, Literal, TypedDict
 
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.data_entry_flow import section
+from homeassistant.helpers.selector import selector
 
+from findmy.accessory import KeyPair
 from findmy.errors import InvalidCredentialsError, UnhandledProtocolError
 from findmy.reports import AsyncAppleAccount, LoginState, RemoteAnisetteProvider
 from findmy.reports.twofactor import (
@@ -27,7 +29,7 @@ if TYPE_CHECKING:
 
 _LOGGER = logging.getLogger(__name__)
 
-DATA_SCHEMA_LOGIN = vol.Schema(
+DATA_SCHEMA_ACC_LOGIN = vol.Schema(
     {
         vol.Required("email"): str,
         vol.Required("password"): str,
@@ -37,16 +39,34 @@ DATA_SCHEMA_LOGIN = vol.Schema(
                     vol.Required("anisette_url", default=DEFAULT_ANISETTE_URL): str,
                 },
             ),
+            {"collapsed": True},
         ),
     },
 )
-DATA_SCHEME_2FA = vol.Schema(
+DATA_SCHEME_ACC_2FA = vol.Schema(
     {
         vol.Required("code"): str,
     },
 )
+ACC_MFA_MENU_CALLBACK_FMT = re.compile(r"^async_step_acc_2fa_request_(\d+)$")
 
-MFA_MENU_CALLBACK_FMT = re.compile(r"^async_step_2fa_request_(\d+)$")
+DATA_SCHEME_DEV_CHOOSE = vol.Schema(
+    {
+        "device_type": selector(
+            {
+                "select": {
+                    "options": ["static"],
+                },
+            },
+        ),
+    },
+)
+DATA_SCHEME_DEV_STATIC = vol.Schema(
+    {
+        vol.Required("name"): str,
+        vol.Required("private_key"): str,
+    },
+)
 
 
 class LoginFlowAdvancedOptions(TypedDict):
@@ -61,13 +81,32 @@ class LoginFlowInput(TypedDict):
     advanced_options: LoginFlowAdvancedOptions
 
 
-class MfaSubmitForm(TypedDict):
+class MfaSubmitInput(TypedDict):
     code: str
 
 
-class EntryData(TypedDict):
+class DeviceTypeInput(TypedDict):
+    device_type: Literal["static"]
+
+
+class StaticDeviceInput(TypedDict):
+    name: str
+    private_key: str
+
+
+class EntryDataAccount(TypedDict):
+    type: Literal["account"]
     account_data: dict[Any, Any]
     anisette_url: str
+
+
+class EntryDataStaticDevice(TypedDict):
+    type: Literal["device_static"]
+    name: str
+    private_key: str
+
+
+type EntryData = EntryDataAccount | EntryDataStaticDevice
 
 
 class InitialSetupConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -88,12 +127,12 @@ class InitialSetupConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     def __getattr__(self, item: str) -> Callable:
         """Catch menu callbacks from the 2FA selection menu."""
-        matches = MFA_MENU_CALLBACK_FMT.fullmatch(item)
+        matches = ACC_MFA_MENU_CALLBACK_FMT.fullmatch(item)
         if not matches:
             raise AttributeError
 
         step_info = {"method_id": int(matches.group(1))}
-        return lambda data: self.async_step_2fa_request({**(data or {}), **step_info})
+        return lambda data: self.async_step_acc_2fa_request({**(data or {}), **step_info})
 
     async def async_step_user(
         self,
@@ -101,14 +140,32 @@ class InitialSetupConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     ) -> config_entries.ConfigFlowResult:
         _LOGGER.debug("%s Step: user - %s", self.__class__.__name__, user_input)
 
-        return self.async_show_form(
-            step_id="login",
-            data_schema=DATA_SCHEMA_LOGIN,
+        return self.async_show_menu(
+            step_id="user",
+            menu_options={
+                "start_acc": "Apple Account",
+                "start_dev": "FindMy Device",
+            },
         )
 
-    async def async_step_login(self, info: LoginFlowInput | None) -> FlowResult:
+    ##########################
+    ### Account Setup Flow ###
+    ##########################
+
+    async def async_step_start_acc(
+        self,
+        user_input: dict[str, Any] | None = None,
+    ) -> config_entries.ConfigFlowResult:
+        _LOGGER.debug("%s Step: start_acc - %s", self.__class__.__name__, user_input)
+
+        return self.async_show_form(
+            step_id="acc_login",
+            data_schema=DATA_SCHEMA_ACC_LOGIN,
+        )
+
+    async def async_step_acc_login(self, info: LoginFlowInput | None) -> FlowResult:
         _LOGGER.debug(
-            "%s Step: login - %s",
+            "%s Step: acc_login - %s",
             self.__class__.__name__,
             {**(info or {}), "password": "**REDACTED**"},
         )
@@ -116,8 +173,8 @@ class InitialSetupConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         if info is None:
             logging.warning("Login step called without info data")
             return self.async_show_form(
-                step_id="login",
-                data_schema=DATA_SCHEMA_LOGIN,
+                step_id="acc_login",
+                data_schema=DATA_SCHEMA_ACC_LOGIN,
             )
 
         await self.async_set_unique_id(info["email"].lower())
@@ -134,8 +191,8 @@ class InitialSetupConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             await self._account.login(info["email"], info["password"])
         except InvalidCredentialsError:
             return self.async_show_form(
-                step_id="login",
-                data_schema=DATA_SCHEMA_LOGIN,
+                step_id="acc_login",
+                data_schema=DATA_SCHEMA_ACC_LOGIN,
                 errors={"base": "invalid_auth"},
             )
         except UnhandledProtocolError:
@@ -145,11 +202,11 @@ class InitialSetupConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         _LOGGER.debug("State after login: %s", self._account.login_state)
 
         if self._account.login_state == LoginState.REQUIRE_2FA:
-            return await self.async_step_2fa_prompt()
-        return await self.async_step_done()
+            return await self.async_step_acc_2fa_prompt()
+        return await self.async_step_acc_done()
 
-    async def async_step_2fa_prompt(self, info: dict | None = None) -> FlowResult:
-        _LOGGER.debug("%s Step: 2fa_prompt - %s", self.__class__.__name__, info)
+    async def async_step_acc_2fa_prompt(self, info: dict | None = None) -> FlowResult:
+        _LOGGER.debug("%s Step: acc_2fa_prompt - %s", self.__class__.__name__, info)
 
         if self._account is None or self._account.login_state != LoginState.REQUIRE_2FA:
             _LOGGER.error("2FA step called but account configured incorrectly")
@@ -160,27 +217,29 @@ class InitialSetupConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         menu_options: dict[str, str] = {}
         for method in self._2fa_methods:
             if isinstance(method, TrustedDeviceSecondFactorMethod):
-                menu_options[f"2fa_request_{len(menu_options)}"] = "Trusted Device"
+                menu_options[f"acc_2fa_request_{len(menu_options)}"] = "Trusted Device"
             elif isinstance(method, SmsSecondFactorMethod):
-                menu_options[f"2fa_request_{len(menu_options)}"] = f"SMS - {method.phone_number}"
+                menu_options[f"acc_2fa_request_{len(menu_options)}"] = (
+                    f"SMS - {method.phone_number}"
+                )
             else:
                 logging.warning("Unknown 2FA method: %s", method)
                 continue
 
         if not menu_options:
             return self.async_show_form(
-                step_id="login",
-                data_schema=DATA_SCHEMA_LOGIN,
+                step_id="acc_login",
+                data_schema=DATA_SCHEMA_ACC_LOGIN,
                 errors={"base": "2fa_unavailable"},
             )
 
         return self.async_show_menu(
-            step_id="2fa_request",
+            step_id="acc_2fa_prompt",
             menu_options=menu_options,
         )
 
-    async def async_step_2fa_request(self, info: dict | None) -> FlowResult:
-        _LOGGER.debug("%s Step: 2fa_request - %s", self.__class__.__name__, info)
+    async def async_step_acc_2fa_request(self, info: dict | None) -> FlowResult:
+        _LOGGER.debug("%s Step: acc_2fa_request - %s", self.__class__.__name__, info)
 
         if info is None:
             _LOGGER.error("2FA requested but no method ID")
@@ -196,26 +255,26 @@ class InitialSetupConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         await self._2fa_method.request()
 
         return self.async_show_form(
-            step_id="2fa_submit",
-            data_schema=DATA_SCHEME_2FA,
+            step_id="acc_2fa_submit",
+            data_schema=DATA_SCHEME_ACC_2FA,
         )
 
-    async def async_step_2fa_submit(self, info: MfaSubmitForm) -> FlowResult:
-        _LOGGER.debug("%s Step: 2fa_submit - %s", self.__class__.__name__, info)
+    async def async_step_acc_2fa_submit(self, info: MfaSubmitInput) -> FlowResult:
+        _LOGGER.debug("%s Step: acc_2fa_submit - %s", self.__class__.__name__, info)
 
         code = info.get("code")
         if not code:
             _LOGGER.error("No 2FA code submitted")
             return self.async_show_form(
                 step_id="2fa_submit",
-                data_schema=DATA_SCHEMA_LOGIN,
+                data_schema=DATA_SCHEMA_ACC_LOGIN,
             )
 
         if self._2fa_method is None:
             _LOGGER.error("No active 2FA method in the flow")
             return self.async_show_form(
                 step_id="2fa_prompt",
-                data_schema=DATA_SCHEMA_LOGIN,
+                data_schema=DATA_SCHEMA_ACC_LOGIN,
             )
 
         try:
@@ -223,26 +282,88 @@ class InitialSetupConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         except UnhandledProtocolError:
             _LOGGER.exception("Unhandled protocol exception during 2FA submit")
             return self.async_show_form(
-                step_id="2fa_prompt",
-                data_schema=DATA_SCHEMA_LOGIN,
+                step_id="acc_2fa_prompt",
+                data_schema=DATA_SCHEMA_ACC_LOGIN,
                 errors={"base": "2fa_invalid"},
             )
 
-        return await self.async_step_done()
+        return await self.async_step_acc_done()
 
-    async def async_step_done(self, info: dict | None = None) -> FlowResult:
-        _LOGGER.debug("%s Step: done - %s", self.__class__.__name__, info)
+    async def async_step_acc_done(self, info: dict | None = None) -> FlowResult:
+        _LOGGER.debug("%s Step: acc_done - %s", self.__class__.__name__, info)
 
         if self._anisette_url is None or self._account is None:
             _LOGGER.exception("No anisette url or account configured")
             return self.async_abort(reason="unknown_error")
 
-        data: EntryData = {
-            "account_data": self._account.export(),
-            "anisette_url": self._anisette_url,
-        }
+        data = EntryDataAccount(
+            type="account",
+            account_data=self._account.export(),
+            anisette_url=self._anisette_url,
+        )
 
         return self.async_create_entry(
-            title=self._account.account_name,
+            title=f"Account: {self._account.account_name}",
+            data=data,
+        )
+
+    #########################
+    ### Device Setup Flow ###
+    #########################
+
+    async def async_step_start_dev(self, info: dict | None = None) -> FlowResult:
+        _LOGGER.debug("%s Step: start_dev - %s", self.__class__.__name__, info)
+
+        return self.async_show_form(
+            step_id="dev_choose",
+            data_schema=DATA_SCHEME_DEV_CHOOSE,
+        )
+
+    async def async_step_dev_choose(self, info: DeviceTypeInput | None = None) -> FlowResult:
+        _LOGGER.debug("%s Step: dev_choose - %s", self.__class__.__name__, info)
+
+        dev_type = (info or {}).get("device_type", None)
+        if dev_type == "static":
+            return self.async_show_form(
+                step_id="dev_static",
+                data_schema=DATA_SCHEME_DEV_STATIC,
+            )
+
+        return self.async_show_form(
+            step_id="dev_choose",
+            data_schema=DATA_SCHEME_DEV_CHOOSE,
+            errors={"base": "invalid_dev"},
+        )
+
+    async def async_step_dev_static(self, info: StaticDeviceInput | None = None) -> FlowResult:
+        _LOGGER.debug("%s Step: dev_static - %s", self.__class__.__name__, info)
+
+        if not info:
+            return self.async_show_form(
+                step_id="dev_static",
+                data_schema=DATA_SCHEME_DEV_STATIC,
+                errors={"base": "invalid_dev"},
+            )
+
+        name = info.get("name", "Unknown")
+        key = info.get("private_key", "")
+
+        try:
+            KeyPair.from_b64(key)
+        except ValueError:
+            return self.async_show_form(
+                step_id="dev_static",
+                data_schema=DATA_SCHEME_DEV_STATIC,
+                errors={"base": "invalid_dev_key"},
+            )
+
+        data = EntryDataStaticDevice(
+            type="device_static",
+            name=name,
+            private_key=key,
+        )
+
+        return self.async_create_entry(
+            title=f"Device (Static): {name}",
             data=data,
         )
