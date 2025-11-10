1b50364e82971d671e4ed2eea055fba510ba6a3f
Mike A. <git@mikealmel.ooo>
2025-11-10 19:45:18 +0100

    Disable timeout when fetching

--- custom_components/findmy/coordinator.py
+++ custom_components/findmy/coordinator.py
@@ -6,7 +6,6 @@ import logging
 from datetime import timedelta
 from typing import TYPE_CHECKING, final, override
 
-import async_timeout
 from homeassistant.exceptions import ConfigEntryAuthFailed
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
@@ -77,8 +76,7 @@ class FindMyCoordinator(DataUpdateCoordinator[FindMyLocationData]):
         devices: list[FindMyDevice] = list(self.async_contexts())
         _LOGGER.debug("Fetching reports for devices: %s", devices)
         try:
-            async with async_timeout.timeout(10):
-                device_reports = await account.fetch_location(devices)
+            device_reports = await account.fetch_location(devices)
         except UnauthorizedError as err:
             _LOGGER.exception("Unauthorized... :c")
             raise ConfigEntryAuthFailed from err
